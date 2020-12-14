bac3ab2675001f4ede511a843426edd5160d8db1
mattmeyerink <meyerink@umich.edu>
2020-12-14 17:42:55 -0500

    Adding beginner marathon plan

---
+++ app/blueprints/training_plans/training_plan_db_entries/beginner_marathon.py
@@ -0,0 +1,237 @@
+"""Script to create beginner marathon training db."""
+from ../models import BeginnerMarathon
+
+
+week1 = BeginnerMarathon()
+week1_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 3,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 6,
+    "sunday": 0
+}
+week1.from_dict(week1_dict)
+week1.save()
+
+week2 = BeginnerMarathon()
+week2_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 3,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 7,
+    "sunday": 0
+}
+week2.from_dict(week2_dict)
+week2.save()
+
+week3 = BeginnerMarathon()
+week3_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 4,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 5,
+    "sunday": 0
+}
+week3.from_dict(week3_dict)
+week3.save()
+
+week4 = BeginnerMarathon()
+week4_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 3,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 6,
+    "sunday": 0
+}
+week4.from_dict(week4_dict)
+week4.save()
+
+week5 = BeginnerMarathon()
+week5_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 4,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 9,
+    "sunday": 0
+}
+week5.from_dict(week5_dict)
+week5.save()
+
+week6 = BeginnerMarathon()
+week6_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 5,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 10,
+    "sunday": 0
+}
+week6.from_dict(week6_dict)
+week6.save()
+
+week7 = BeginnerMarathon()
+week7_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 5,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 7,
+    "sunday": 0
+}
+week7.from_dict(week7_dict)
+week7.save()
+
+week8 = BeginnerMarathon()
+week8_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 6,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 0,
+    "sunday": 13
+}
+week8.from_dict(week8_dict)
+week8.save()
+
+week9 = BeginnerMarathon()
+week9_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 7,
+    "thursday": 4,
+    "friday": 0,
+    "saturday": 10,
+    "sunday": 0
+}
+week9.from_dict(week9_dict)
+week9.save()
+
+week10 = BeginnerMarathon()
+week10_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 7,
+    "thursday": 4,
+    "friday": 0,
+    "saturday": 15,
+    "sunday": 0
+}
+week10.from_dict(week10_dict)
+week10.save()
+
+week11 = BeginnerMarathon()
+week11_dict = {
+    "monday": 0,
+    "tuesday": 4,
+    "wednesday": 8,
+    "thursday": 4,
+    "friday": 0,
+    "saturday": 16,
+    "sunday": 0
+}
+week11.from_dict(week11_dict)
+week11.save()
+
+week12 = BeginnerMarathon()
+week12_dict = {
+    "monday": 0,
+    "tuesday": 4,
+    "wednesday": 8,
+    "thursday": 5,
+    "friday": 0,
+    "saturday": 12,
+    "sunday": 0
+}
+week12.from_dict(week12_dict)
+week12.save()
+
+week13 = BeginnerMarathon()
+week13_dict = {
+    "monday": 0,
+    "tuesday": 4,
+    "wednesday": 9,
+    "thursday": 5,
+    "friday": 0,
+    "saturday": 18,
+    "sunday": 0
+}
+week13.from_dict(week13_dict)
+week13.save()
+
+week14 = BeginnerMarathon()
+week14_dict = {
+    "monday": 0,
+    "tuesday": 5,
+    "wednesday": 9,
+    "thursday": 5,
+    "friday": 0,
+    "saturday": 14,
+    "sunday": 0
+}
+week14.from_dict(week14_dict)
+week14.save()
+
+week15 = BeginnerMarathon()
+week15_dict = {
+    "monday": 0,
+    "tuesday": 5,
+    "wednesday": 10,
+    "thursday": 5,
+    "friday": 0,
+    "saturday": 20,
+    "sunday": 0
+}
+week15.from_dict(week15_dict)
+week15.save()
+
+week16 = BeginnerMarathon()
+week16_dict = {
+    "monday": 0,
+    "tuesday": 5,
+    "wednesday": 8,
+    "thursday": 4,
+    "friday": 0,
+    "saturday": 12,
+    "sunday": 0
+}
+week16.from_dict(week16_dict)
+week16.save()
+
+week17 = BeginnerMarathon()
+week17_dict = {
+    "monday": 0,
+    "tuesday": 4,
+    "wednesday": 6,
+    "thursday": 3,
+    "friday": 0,
+    "saturday": 8,
+    "sunday": 0
+}
+week17.from_dict(week17_dict)
+week17.save()
+
+week18 = BeginnerMarathon()
+week18_dict = {
+    "monday": 0,
+    "tuesday": 3,
+    "wednesday": 4,
+    "thursday": 2,
+    "friday": 0,
+    "saturday": 0,
+    "sunday": 26
+}
+week18.from_dict(week18_dict)
+week18.save()
--- app/blueprints/training_plans/training_plan_db_entries/training_plans.py
+++ app/blueprints/training_plans/training_plan_db_entries/training_plans.py
@@ -6,8 +6,30 @@ marathon_easy_dict = {
     "race_length": 26.2,
     "race_name": "marathon",
     "difficulty": "beginner",
-    "frequency": 5,
-    "plan_length": 16
+    "frequency": 4,
+    "plan_length": 18
 }
 marathon_easy.from_dict(marathon_easy_dict)
 marathon.save()
+
+marathon_intermediate = TrainingPlan()
+marathon_intermediate_dict = {
+    "race_length": 26.2,
+    "race_name": "marathon",
+    "difficulty": "intermediate",
+    "frequency": 6,
+    "plan_length": 16
+}
+marathon_intermediate.from_dict(marathon_intermediate_dict)
+marathon_intermediate.save()
+
+marathon_advanced = TrainingPlan()
+marathon_advanced_dict = {
+    "race_length": 26.2,
+    "race_name": "marathon",
+    "difficulty": "beginner",
+    "frequency": 7,
+    "plan_length": 16
+}
+marathon_advanced.from_dict(marathon_advanced_dict)
+marathon_advanced.save()
